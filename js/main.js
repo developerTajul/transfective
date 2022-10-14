23ce3512d716f363ec866ae22338b739f69911b6
Sakil Anwar <sakilanwar60@gmail.com>
2022-10-14 12:40:49 +0600

    verticle tab

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-(function($){
+(function ($) {
 
-	jQuery(document).ready(function(){
+	jQuery(document).ready(function () {
+
+
+		// slick slider
+		$('#industriesTab').slick({
+			dots: false,
+			infinite: false,
+			speed: 300,
+			slidesToShow: 3,
+			arrows: false,
+			variableWidth: true,
+
+		});
+
 
 		/*** Sticky header */
-		$(window).scroll(function(){
-			if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
+		$(window).scroll(function () {
+			if ($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
 				$(".header").addClass("stop");
 			} else {
 				$(".header").removeClass("stop");
@@ -19,20 +32,17 @@
 
 		window.addEventListener("scroll", () => {
 			const currentScroll = window.pageYOffset;
-			if (currentScroll <= 0) 
-			{
+			if (currentScroll <= 0) {
 				body.classList.remove(scrollUp);
 				return;
 			}
 
-			if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) 
-			{
+			if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
 				// down
 				body.classList.remove(scrollUp);
 				body.classList.add(scrollDown);
-			} 
-			else if ( currentScroll < lastScroll && body.classList.contains(scrollDown) ) 
-			{
+			}
+			else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
 				// up
 				body.classList.remove(scrollDown);
 				body.classList.add(scrollUp);
@@ -43,27 +53,27 @@
 
 		/*** Scroll Nav */
 		var link = $('.main-menu ul li a');
- 
-		link.on('click', function(e) {
+
+		link.on('click', function (e) {
 			var target = $($(this).attr('href'));
 			$('html, body').animate({
-			scrollTop: target.offset().top - 76
-			}, 600); 
+				scrollTop: target.offset().top - 76
+			}, 600);
 			$(this).parent().addClass('active');
 			e.preventDefault();
 		});
- 
-		$(window).on('scroll', function(){
+
+		$(window).on('scroll', function () {
 			scrNav();
 		});
 
 		function scrNav() {
 			var sTop = $(window).scrollTop();
-			$('section').each(function() {
+			$('section').each(function () {
 				var id = $(this).attr('id'),
-					offset = $(this).offset().top-1,
+					offset = $(this).offset().top - 1,
 					height = $(this).height();
-				if(sTop >= offset && sTop < offset + height) {
+				if (sTop >= offset && sTop < offset + height) {
 					link.parent().removeClass('active');
 					$('.main-menu').find('[href="#' + id + '"]').parent().addClass('active');
 				}
@@ -78,24 +88,24 @@
 		});
 
 		/*** ScrollDown */
-        $('.scrollDown').click(function() {
-            var target = $($(this).data('target'));
-            var space = $(this).data('space');
+		$('.scrollDown').click(function () {
+			var target = $($(this).data('target'));
+			var space = $(this).data('space');
 
-            if (target.length) {
-                $('html,body').animate({
-                scrollTop: target.offset().top - space
-                }, 500);
-            }
-        });
-    	 
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - space
+				}, 500);
+			}
+		});
 
-		$(window).scroll(function(){
-			if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
+
+		$(window).scroll(function () {
+			if ($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
 				$(".side-info").removeClass("info-open");
 				$(".offcanvas-overlay").removeClass("overlay-open");
 			}
-		});  
+		});
 
 		/*** AOS */
 		AOS.init({
@@ -103,7 +113,7 @@
 			offset: -300,
 			duration: 900,
 		});
-		 
+
 	});
 
 }(jQuery));
