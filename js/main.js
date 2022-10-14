f3dc0689659488ead0c1465019ec8dfb6f676e35
Sakil Anwar <sakilanwar60@gmail.com>
2022-10-14 19:20:33 +0600

    home page added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+
+
 (function ($) {
 
 	jQuery(document).ready(function () {
@@ -130,3 +133,20 @@
 	});
 
 }(jQuery));
+
+
+// header
+
+const openMenuBtn = document.querySelector('.header-area .open-menu-btn button');
+const mobileMenu = document.querySelector('.header-area .mobile-menu-area');
+const closeBtn = document.querySelector('.header-area .close-btn button');
+const body = document.querySelector('body');
+
+openMenuBtn.addEventListener('click', function () {
+	mobileMenu.classList.add('toggle-mobile-menu');
+	body.classList.add('bg-gray');
+})
+closeBtn.addEventListener('click', function () {
+	mobileMenu.classList.remove('toggle-mobile-menu');
+	body.classList.remove('bg-gray');
+})
