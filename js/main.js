caf636912fe93df4cd8b16544afdf419a1a26249
Sakil Anwar <sakilanwar60@gmail.com>
2022-10-18 13:45:56 +0600

    home page done

--- js/main.js
+++ js/main.js
@@ -73,7 +73,25 @@
 			slidesToShow: 3,
 			slidesToScroll: 1,
 			arrows: false,
-			variableWidth: true
+			variableWidth: true,
+			responsive: [
+				{
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+					}
+				}
+				
+				
+			]
 		});
 
 
