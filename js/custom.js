6a128821a8cfc9a245509090c0b3bad8f69e819b
Sakil Anwar <sakilanwar60@gmail.com>
2022-11-04 00:00:41 +0600

    bannar arrow

--- js/custom.js
+++ js/custom.js
@@ -37,10 +37,8 @@ const worldAreSmall = document.querySelector('.world-wide-area-small');
 const worldAreaSmallCircleOuter = worldAreSmall.querySelectorAll('.circle-outer');
 worldAreaSmallCircleOuter[1].classList.add('active');
 
-
-
-
 // need project slider
+
 var swiper = new Swiper(".need-project-swiper-slider", {
     direction: "horizontal",
     slidesPerView: 1,
@@ -51,3 +49,38 @@ var swiper = new Swiper(".need-project-swiper-slider", {
         clickable: true,
     },
 });
+
+
+
+// animation
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+
+gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+
+const ball = document.querySelector(".ball");
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const mouse = { x: pos.x, y: pos.y };
+const speed = 0.2;
+
+const xSet = gsap.quickSetter(ball, "x", "px");
+const ySet = gsap.quickSetter(ball, "y", "px");
+
+window.addEventListener("mousemove", e => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+gsap.ticker.add(() => {
+
+    // adjust speed for higher refresh monitors
+    const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+    pos.x += (mouse.x - pos.x) * dt;
+    pos.y += (mouse.y - pos.y) * dt;
+    xSet(pos.x);
+    ySet(pos.y);
+});
+
+// custom animation
